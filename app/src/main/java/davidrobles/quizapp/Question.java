ef1372cdb328a0de3a0ebb7cd7e0d3e529c6958d
David Robles <davidrobles1214@gmail.com>
2019-07-01 20:55:24 -0700

    Initial commit. Finished independent challenges

---
+++ app/src/main/java/davidrobles/quizapp/Question.java
@@ -0,0 +1,72 @@
+package davidrobles.quizapp;
+
+public class Question
+{
+    private int mTextResId;
+    private int mHintTextResId;
+
+    public Question(int textResId, int hintTextResId)
+    {
+        mTextResId = textResId;
+        mHintTextResId = hintTextResId;
+    }
+
+    public int getTextResId()
+    {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId)
+    {
+        mTextResId = textResId;
+    }
+
+    public int getHintTextResId() {
+        return mHintTextResId;
+    }
+
+    public void setHintTextResId(int hintTextResId)
+    {
+        mHintTextResId = hintTextResId;
+    }
+
+    //Stub method - intentionally does nothing
+    //only applies to True false Questions
+    public boolean checkAnswer(boolean boolResponse)
+    {
+        return false;
+    }
+
+    //Stub method
+    //only applies to fill the blank question
+    public boolean checkAnswer(String userAnswer)
+    {
+        return false;
+    }
+
+    //Stub
+    public boolean checkAnswer(int ans)
+    {
+        return false;
+    }
+
+    // Stub
+    public boolean isTrueFalseQuestion()
+    {
+        return false;
+    }
+
+    // Stub
+    public boolean isFillTheBlankQuestion()
+    {
+        return false;
+    }
+
+    // Stub
+    public boolean isMultipleChoiceQuestion()
+    {
+        return false;
+    }
+
+}
+
